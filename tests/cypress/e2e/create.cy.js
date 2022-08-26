a23c9731154236ecd07f2208d2c58254b2f0a984
heidy.oliveira <heidy.oliveira@digicade.com.br>
2022-08-26 15:35:50 -0300

    Desafio

--- tests/cypress/e2e/create.cy.js
+++ tests/cypress/e2e/create.cy.js
@@ -34,4 +34,55 @@ describe('Recomendação', () => {
         
     })
 
+    it('não deve cadastrar foodtruck com o nome duplicado', () => {
+        // 1 - a massa de testes deve ser independentes
+        // 2 - latitude e longitude deve ser única
+        // 3 - você deve encontrar e corrigir o bug nesse cenário
+        
+        const user = {
+            name: 'Benson',
+            instagram: '@benson',
+            password: '1305'
+        }
+
+        const foodtruck = {
+            latitude: '-19.939242655480857',
+            longitude: '-43.938765227794654',
+            name: 'Tienda Del Chavo',            
+            details: 'O melhor lugar para tomar o suco de limão que parece de groselha e tem sabor de tamarindo',
+            opening_hours: 'das 14h às 20h',
+            open_on_weekends: true
+
+        }
+
+        //cy.apiCreateUser(user)
+        cy.uiLogin(user)       
+
+        mapPage.createLink()
+        createPage.form(foodtruck)
+        createPage.submit()
+        createPage.modal.haveText('Esse food fruck já foi cadastrado!')
+    })
+
+    it.only('todos os campos são obrigatórios', () => {
+        const user = {
+            name: 'Benson',
+            instagram: '@benson',
+            password: '1305'
+        }
+
+        const foodtruck = {
+            latitude: '-19.939242655480857',
+            longitude: '-43.938765227794654',            
+        }
+
+        cy.apiCreateUser(user)
+        cy.uiLogin(user)       
+
+        mapPage.createLink()
+        createPage.form(foodtruck)
+        createPage.submit()
+        createPage.modal.haveText('O campos nome, descrição e horário de funcionamento devem ser informados para recomendar um food truck!')
+    })
+
 })
--- tests/cypress/support/pages/Create/index.js
+++ tests/cypress/support/pages/Create/index.js
@@ -9,9 +9,9 @@ class CreatePage {
 
     form(foodtruck) {
         cy.setGeolocation(foodtruck.latitude, foodtruck.longitude)
-        cy.get('input[name=name').type(foodtruck.name)
-        cy.get('textarea[name=details]').type(foodtruck.details)
-        cy.get('input[name=opening-hours]').type(foodtruck.opening_hours)
+        if (foodtruck.name) cy.get('input[name=name').type(foodtruck.name)
+        if (foodtruck.details) cy.get('textarea[name=details]').type(foodtruck.details)
+        if (foodtruck.opening_hours) cy.get('input[name=opening-hours]').type(foodtruck.opening_hours)
 
         //Perguntar se o objeto existe independente do valor que ele tem, se o objeto devolve o texto Sim, se ele não existir devolve o texto 'Não'
         cy.contains('button', foodtruck.open_on_weekends ? 'Sim' : 'Não').click()
